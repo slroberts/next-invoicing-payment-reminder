2884fbc9aa1ec3ef2bf3afd580f75cd410cd6686
Shomari Roberts <shomariroberts@gmail.com>
2022-08-22 21:26:02 -0400

    changed @name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className='py-4 text-slate-400'>
-      built by: <span className='font-semibold'>@shomariroberts</span>
+      built by: <span className='font-semibold'>@slroberts</span>
     </footer>
   );
 }
