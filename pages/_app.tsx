003aebb2966577cc500ee105775560c9d2fa4c0b
Shomari Roberts <shomariroberts@gmail.com>
2022-08-25 22:59:57 -0400

    created and wired up context for session and status

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import SessionContext from '../components/SessionContext';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const [loginSession, setLoginSession] = useState();
+  const [loginStatus, setLoginStatus] = useState();
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <SessionContext.Provider
+        value={{ loginSession, setLoginSession, loginStatus, setLoginStatus }}
+      >
+        <Component {...pageProps} />
+      </SessionContext.Provider>
     </SessionProvider>
   );
 }
