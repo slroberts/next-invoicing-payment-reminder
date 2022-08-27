663be96b09dbe109a0f46e047eb709745524940f
Shomari Roberts <shomariroberts@gmail.com>
2022-08-26 22:44:37 -0400

    changed transition effect

--- components/Button.tsx
+++ components/Button.tsx
@@ -17,7 +17,7 @@ const Button = forwardRef(
         type={type}
         ref={ref}
         onClick={onClick}
-        className={`px-10 py-3 rounded-3xl bg-gradient-to-r from-cyan-500 to-blue-500 text-white font-medium hover:-hue-rotate-60 transition-all duration-300 ease-in ${customStyle}`}
+        className={`px-10 py-3 rounded-3xl bg-gradient-to-r from-cyan-500 to-blue-500 text-white font-medium hover:-hue-rotate-60 transition-colors ${customStyle}`}
       >
         {buttonText}
       </button>
