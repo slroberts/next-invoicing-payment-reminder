a5ab881f8155cecdb5d0f6b7d855ac2250b2163d
Shomari Roberts <shomariroberts@gmail.com>
2022-09-16 16:27:10 -0400

    prisma path updated

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from '../../../lib/prisma';
+import prisma from '../../../prisma/lib/prisma';
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
--- prisma/Client.js
+++ prisma/Client.js
@@ -1,4 +1,4 @@
-import prisma from '../lib/prisma';
+import prisma from './lib/prisma';
 
 export const getClientById = async (id) => {
   const client = await prisma.client.findUnique({
