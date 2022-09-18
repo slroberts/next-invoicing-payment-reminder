17bc0ee1124935c59d57f3b04ed671d8a187ea05
Shomari Roberts <shomariroberts@gmail.com>
2022-09-18 19:53:14 -0400

    updated useForm

--- utils/useForm.ts
+++ utils/useForm.ts
@@ -6,19 +6,39 @@ interface FormValues {
 
 export default function useForm(callback: any, initialState = {}) {
   const [formValues, setFormValues] = useState<FormValues>(initialState);
+  const [open, setOpen] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+
+  const toggleModal = () => {
+    setOpen(!open);
+    setFormValues(initialState);
+  };
 
   const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setFormValues({
       ...formValues,
       [e.currentTarget.name]: e.currentTarget.value,
     });
+
+    if (Object.keys(formValues).length !== 0) {
+      setDisabled(false);
+    }
   };
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
     await callback();
     setFormValues(initialState);
+    setOpen(false);
   };
 
-  return { formValues, handleInputChange, handleSubmit };
+  return {
+    disabled,
+    formValues,
+    open,
+    setOpen,
+    toggleModal,
+    handleInputChange,
+    handleSubmit,
+  };
 }
